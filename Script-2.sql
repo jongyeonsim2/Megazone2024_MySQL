5d5b9ae80a042db81619d43eca4b9a321b7d15a4
unknown <jongyeonsim2@gmail.com>
2024-06-13 14:20:28 +0900

    8th commit

--- Script-2.sql
+++ Script-2.sql
@@ -92,7 +92,22 @@ select *
 
 
 
-/* ================== 다중 테이블 활용 ================== */
+/* ================== 다중 테이블 활용 ================== 
+ * 
+ * 카테시안 product(데카르트 곱)
+ * 
+ * 2개 이상의 테이블을 사용해서 데이터를 조회시,
+ * 두 테이블의 관계를 설정하지 않고 조회를 하면,
+ * 서로 연결할 수 있는 조건으로 연결된 모든 데이터가 조회됨.
+ * 
+ * 일반적으로는 전혀 의미가 없지만,
+ * 특별한 경우( 테스트 데이터, dummy 데이터 용 )는 사용 할 수 있음.
+ * 
+ * 따라서, 반드시 두 테이블간의 관계 설정을 해야 함.
+ * 
+ * 
+ * 
+ * */
 
 /* self join 실습 용 table 및 데이터 
  * 
@@ -156,6 +171,93 @@ select count(*) from emp;  => 총 row 수 : 14 건 을 확인
  * */
  
  
+ 
+/* 데카르트 곱 */ 
+select count(*) from customer; /* 599 */
+select count(*) from address; /* 603 */
+
+select count(*) 
+  from customer c join address a; /* 361,197 */
+
+/* 한 번 해보세요. 
+ * 599, 603, 361197 결과가 한 번의 SQL 실행으로 모두 조회가 되도록
+ * */
+select customer_cnt.cnt, address_cnt.cnt, cartesian_cnt.cnt
+  from 
+  	(select count(*) cnt from customer) customer_cnt,
+  	(select count(*) cnt from address) address_cnt,
+  	(select count(*) cnt
+       from customer c join address a) cartesian_cnt;
+
+/* 데카르트 곱을 회피 => 관계 설정을 하면 됨. => address_id 로 관계 설정 */
+select c.first_name , c.last_name , a.address 
+  from customer c join address a 
+    on c.address_id = a.address_id ;
+      
+/* 실행하면 599 건이 확인됨.
+ * 
+ * 599 건은 현재 고객수는 599임.
+ * 고객이 599 명인데, 아래의 문장을 실행해서,
+ * 598 건 이 나온다면, 주소 마스터 테이블의 주소 정보에 문제가
+ * 있다고 생각하고, 확인을 해야 함.
+ * 
+ *  */
+select count(*)
+  from customer c join address a 
+    on c.address_id = a.address_id ;
+
+
+/* ANSI(표준) join 문법 - SQL92 버전 */
+select c.first_name , c.last_name , a.address 
+  from customer c join address a 
+    on c.address_id = a.address_id 
+ where a.postal_code = 52137;
+
+/* 비 ANSI(비표준) 
+ * 
+ * 비표준 SQL 에는 벤더 지향 SQL이 있음. => 고급 기능임.
+ * */
+select c.first_name , c.last_name , a.address 
+  from customer c, address a
+ where c.address_id = a.address_id; 
+
+select c.first_name , c.last_name , a.address 
+  from customer c, address a
+ where c.address_id = a.address_id
+   and a.postal_code = 52137; 
+  
+/*
+ * 표준SQL 의 장점
+ * - 조인 조건과 추가적인 필터 조건의 구분이 되어, 가독성이 높음.
+ * - 조인 조건에 대한 누락될 가능성이 낮음.
+ * - 표준 SQL 다른 벤더의 데이터베이스로의 이식성이 높음.
+ * 
+ * */
+
+
+select count(*)
+  from customer c, address a
+ where c.address_id = a.address_id; 
+
+
+/* 
+ * 세 테이블 조인.
+ * 
+ * 고객명, 도시명 조회.
+ * customer, city, address
+ * 
+ * 1. 표준 SQL 사용해서 3테이블 관계 맺도록 해서 SQL 작성.
+ * 2. 서브쿼리를 활용.
+ *    큰 틀에서는 마치 테이블 2개로 관계를 맺는것처럼 SQL 을 작성.
+ * 3. 그래서, 둘 중 어느 sql 이 가독성이 좋은지 비교.
+ * 4. 2번에서 서브쿼리로 사용한 SQL을 view 로 생성.
+ *    서브쿼리 대신에 view 를 사용한 SQL 로 작성.
+ * 
+ * 패턴이 총 3가지임. 여기서 어떤 부분이 가독성과 유지보수성이 좋은지 비교.
+ * 
+ * */
+
+select * from address;
 
 
  				
