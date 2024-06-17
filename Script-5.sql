2cb902536762f1dfb98cfa2b4d4b3673cb3ad06f
unknown <jongyeonsim2@gmail.com>
2024-06-17 16:11:52 +0900

    14th commit

--- Script-5.sql
+++ Script-5.sql
@@ -490,11 +490,49 @@ select c.first_name , c.last_name , ct.city ,
    
    
    
-/* 성이 'S'로 시작하는 배우가 출연하는 PG 등급 영화 대여로 발생한
+/* CTE 를 사용하면 좋은 경우.
+ * 
+ * 성이 'S'로 시작하는 배우가 출연하는 PG 등급 영화 대여로 발생한
  * 총 수익(대여료)을 조회 
  * 
  * 영화 배우명(first_name, last_name), 총 수익 으로 조회.
  * */
+   
+
+WITH actors_s AS
+      (SELECT actor_id, first_name, last_name
+       FROM actor
+       WHERE last_name LIKE 'S%'
+      ),
+      actors_s_pg AS
+      (SELECT s.actor_id, s.first_name, s.last_name,
+         f.film_id, f.title
+       FROM actors_s s
+         INNER JOIN film_actor fa
+         ON s.actor_id = fa.actor_id
+         INNER JOIN film f
+         ON f.film_id = fa.film_id
+       WHERE f.rating = 'PG'
+      ),
+      actors_s_pg_income AS
+      (SELECT spg.first_name, spg.last_name, p.amount
+       FROM actors_s_pg spg
+         INNER JOIN inventory i
+         ON i.film_id = spg.film_id
+         INNER JOIN rental r
+         ON i.inventory_id = r.inventory_id
+         INNER JOIN payment p
+         ON r.rental_id = p.rental_id
+      ) -- end of With clause
+     SELECT spg_income.first_name, spg_income.last_name,
+       sum(spg_income.amount) tot_revenue
+     FROM actors_s_pg_income spg_income
+     GROUP BY spg_income.first_name, spg_income.last_name
+     ORDER BY 3 desc;
+   
+   
+   
+   
    
    
 /* 영화 배우를 조회. 영화 배우 ID, 영화 배우명(first_name, last_name) 
