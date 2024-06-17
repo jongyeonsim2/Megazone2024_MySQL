b0978d998c12a08973e3f92ba1c833c3c793ba0a
unknown <jongyeonsim2@gmail.com>
2024-06-17 16:14:14 +0900

    15th commit

--- Script-5.sql
+++ Script-5.sql
@@ -558,4 +558,41 @@ WITH actors_s AS
  *   film id 는 13, 14, 15 로 한정. 
  * */
   
- 							
+ 		
+/* 0 ~ 399 */    
+SELECT ones.num + tens.num + hundreds.num
+ FROM
+ (SELECT 0 num UNION ALL
+ SELECT 1 num UNION ALL
+ SELECT 2 num UNION ALL
+ SELECT 3 num UNION ALL
+ SELECT 4 num UNION ALL
+ SELECT 5 num UNION ALL
+ SELECT 6 num UNION ALL
+ SELECT 7 num UNION ALL
+ SELECT 8 num UNION ALL
+ SELECT 9 num) ones
+ CROSS JOIN
+ (SELECT 0 num UNION ALL
+ SELECT 10 num UNION ALL
+ SELECT 20 num UNION ALL
+ SELECT 30 num UNION ALL
+ SELECT 40 num UNION ALL
+ SELECT 50 num UNION ALL
+ SELECT 60 num UNION ALL
+ SELECT 70 num UNION ALL
+ SELECT 80 num UNION ALL
+ SELECT 90 num) tens
+ CROSS JOIN
+ (SELECT 0 num UNION ALL
+ SELECT 100 num UNION ALL
+ SELECT 200 num UNION ALL
+ SELECT 300 num) hundreds;
+    
+/*
+ * 
+ * 
+ * 
+ */    
+    
+    
